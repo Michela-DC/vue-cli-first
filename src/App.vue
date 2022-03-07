594ae9d68f138cdc8f6645023e6dc83e8d7c713c
Michela-DC <micheladelconte@outlook.com>
2022-03-07 18:29:11 +0100

    Added Title

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TitleVueCli/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TitleVueCli from './components/TitleVueCli.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TitleVueCli
   }
 }
 </script>
---
+++ src/components/TitleVueCli.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+  name: 'TitleVueCli',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1{
+    background-color: red;
+    color: white;
+}
+</style>
